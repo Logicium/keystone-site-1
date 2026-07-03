1c072b7be54951c556a9e3dfca850bc5456937be
Kisora Thomas <kisora.thomas@gmail.com>
2026-07-02 23:02:11 -0600

    chore: sync from template 8bc8ec0

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { siteConfig } from '../config/site.config'
-import HeroSection from '../components/sections/HeroSection.vue'
+import HeroSection from '@apotome/archetype-shared/components/sections/HeroSection.vue'
 import ServicesSection from '../components/sections/ServicesSection.vue'
 import CapabilitiesSection from '../components/sections/CapabilitiesSection.vue'
 import DispatchBar from '../components/sections/DispatchBar.vue'
